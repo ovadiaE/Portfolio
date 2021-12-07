5d0c71fdf66d1d3e3b612e3ac313ed28763e970f
Ovadia Esquenazi <ovadia.esq@gmail.com>
2021-12-07 12:32:27 -0600

    added grid

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,20 +1,37 @@
 import { useEffect }from 'react'
 import styles from './About.module.scss'
+import Grid from '@mui/material/Grid'
+import CodeSharpIcon from '@mui/icons-material/CodeSharp'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 
-const Navbar = () => {
+const About = () => {
 
     useEffect(()=>{Aos.init({duration: 2000})},[])
-    
-    return ( 
+
+    const header = () => (
         <div className={styles['wrapper']}>
-            <div data-aos='fade' className={styles['header']}>
-                About
-            </div>
-            
+            <div data-aos='fade' className={styles['header']}>About</div>
+            <Grid data-aos='fade' container spacing={6} className={styles['buzzword-grid']}>
+                <Grid item className={styles['buzzword-item']}> {iconDiv(<CodeSharpIcon/>)} </Grid>
+                <Grid item className={styles['buzzword-item']}> <item>Hello</item> </Grid>
+                <Grid item className={styles['buzzword-item']}> <item>Hello</item> </Grid>
+                <Grid item className={styles['buzzword-item']}> <item>Hello</item> </Grid>
+            </Grid>
         </div>
+     )
+
+     const iconDiv = (icon) => (
+        <item>
+            <div className={styles['icon']}>
+                {icon}
+            </div>
+        </item>
+     )
+     
+     return ( 
+         header()
     )
 }
 
-export default Navbar
+export default About
